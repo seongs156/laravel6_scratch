8f4fee58bd78e30fe07164e23737214860eea092
ksh <seongs156@gmail.com>
2022-12-27 18:08:58 +0900

    Routing to Controllers

--- routes/web.php
+++ routes/web.php
@@ -18,17 +18,5 @@ Route::get('/', function() {
 });
 
 
-Route::get('/posts/{post}', function ($post) {
-    $posts = [
-      'my-first-post' => "Hello this is my first blog post!!",
-        'my-second-post' => "Now I am getting the hang of this bloggin thing."
-    ];
 
-    if(! array_key_exists($post, $posts)) {
-        abort(404, 'Sorry, that post was not found.');
-    }
-
-    return view('post',[
-        'post' => $posts[$post]
-    ]);
-});
+Route::get('posts/{post}', 'PostsController@show');
