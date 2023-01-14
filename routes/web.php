6cc8e10964ae9f2ff4d48ad9f14fca4b7d90f9a6
ksh <seongs156@gmail.com>
2023-01-14 10:54:54 +0900

    Layout Pages

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
 
 Route::get('articles', 'ArticlesController@index');
 Route::get('articles/{article}', 'ArticlesController@show');
