ee8d916509a3473b33eeae84a54e6c48f69d8562
ksh <seongs156@gmail.com>
2023-01-15 10:39:19 +0900

    Render Dynamic Data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/about', function () {
-    return view('about');
+    return view('about', [
+        'articles' =>  App\Article::take(3)->latest()->get()
+    ]);
 });
